ee79dabdc196eccdf78fc9ca88a4b42ebe12dddb
USER <martinpatriarca@gmail.com>
2024-04-09 19:29:03 -0300

    Modificado 09/04

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Grego/CLionProjects/untitled
+# Install script for directory: C:/Users/USER/CLionProjects/VM1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Grego/CLionProjects/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/USER/CLionProjects/VM1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+//#include "Nmemonicos.h"
 #define N 16384
+
 typedef struct {
-  char bit[8];
+    unsigned char byte;
 } reg;
 
 typedef struct{
-    char Identificador[5];
-    char Version[1];
-    int Tamano;
+    char identificador[5];
+    char version[1];
+    long int tamano;
 } header;
 
-
-void CargarArchivo(FILE *nombrearchivo, reg *memoria[N], int *DS, header *Header);
+void ConvertidorBinario(int n, int []);
+void CargarArchivo(FILE *nombrearchivo, reg memoria[N], int *DS, header *Header);
 void LeerMemoria(reg memoria[N], int DS);
 int getFileSize (FILE* file);
 
@@ -27,39 +29,45 @@ int main() {
     else {
         CargarArchivo(Archivo, memoria, &DS,&Header);
         fclose(Archivo);
-
+        LeerMemoria(memoria,DS);
     }
     return 0;
 }
 
-void CargarArchivo(FILE *nombrearchivo, reg * memoria[N], int *DS,header *Header){
-    int i=0,j=0;
+void CargarArchivo(FILE *nombrearchivo, reg memoria[N], int *DS,header *Header){
+    int i=0,j;
     int tamano=getFileSize(nombrearchivo);
     unsigned char data[tamano];
-          fread(data, sizeof (char), tamano  , nombrearchivo);
-    for (int i = 0; i < 5; ++i) {
-        Header->Identificador[i] = data[i];
-        printf("%c \n", data[i]);
+    fread(data, sizeof (char), tamano  , nombrearchivo);
+    for (int i = 0; i < 5; i++) {
+        Header->identificador[i] = data[i];
+        printf("%c \n", data[i]);     //identificador
     }
     for (int i=5; i < 6; ++i) {
-        Header->Version[0] = data[i];
-        printf("%d \n",data[i]);
+        Header->version[0] = data[i];
+        printf("%d \n",data[i]);     //version
     }
-    for (int i=6; i < 8; ++i)
-        printf("%d \n",data[i]);
-    for (int i=8; i < tamano; ++i) {
-        printf("%02X \n",data[i]);
+    Header->tamano = data[6] * 256 + data[7];
+    printf("%d \n",Header->tamano);
+
+
+    printf("Memoria: \n");
+    j=0;
+    for (int i=8; i < tamano; ++i) {        // Cargando la memoria con instrucciones
+        printf("%c ",data[i]);
+        memoria[j].byte=data[i];
+        printf("%04X: %02X \n", i - 8, memoria[j].byte);
+        j++;
     }
+    *DS=Header->tamano;
     printf("Carga completa %d \n",*DS);
 }
 
 void LeerMemoria(reg memoria[N], int DS){
     int j;
-    for (int i = 0; i < DS; ++i)
-    {
-        for (j=0 ; j<8;j++)
-            printf("%d %c \n", i, (memoria[i]).bit[j]);
-        }
+    for (int i = 0; i < DS; ++i){
+        printf("%d %02X \n", i, memoria[i]);
+    }
 }
 
 int getFileSize (FILE* file){
@@ -67,4 +75,84 @@ int getFileSize (FILE* file){
     int size= ftell(file);
     fseek(file,0,SEEK_SET);
     return size;
+}
+//            ConvertidorBinario(memoria[i], &byte)
+//            if (byte[0]==1)
+void ConvertidorBinario(int n, int byte[]){
+    int aux=0;
+    int a[8],i;
+    for(int i=0;n>0;i++)
+    {
+        a[i]=n%2;
+        n=n/2;
+    }
+    for (int j = i-1; j>=0 ; --j) {
+        byte[aux]=a[j];
+    }
+}
+
+int CantOP(int byte[]){
+    int entero;
+    int i,j=8;
+    for (i=0;i<3;i++){
+        entero=byte[i]*j;
+        j/=2;
+    }
+    if (entero==15)
+        return 0;
+    else
+    if (!(entero%2))
+        return 2;
+    else
+        return 1;
+
+}
+
+int CantB(int bitizq,int bitder){
+    int i;
+    if (bitizq)
+        if (bitder)
+            return 0;
+        else
+            return 1;
+    else
+    if (bitder)
+        return 2;
+    else
+        return 3;
+}
+
+int Nfunc(int byte[]){
+    int i,numero=0,j=1;
+    for (i=0;i<3;i++){
+        numero+=j*byte[i];
+        j*=2;
+    }
+    return numero;
+}
+
+void OperandoDeMemoria(int byte1[], int byte2, int byte3, int *PosReg, int ){
+    *PosReg= Nfunc(byte1);
+    OperandoInmediato(byte2,byte3);
+}
+
+int OperandoInmediato(int byte1, int byte2){
+    return byte1*256+byte2;
+}
+
+void OperandoRegistro(int byte[], char Reg[], int *PosReg) {
+    if (byte[5]) {
+        if (byte[4]) {
+            strcpy(Reg,"AX");
+        } else
+            strcpy(Reg,"AH");
+    }
+    else {
+        if (byte[4]) {
+            strcpy(Reg,"AL");
+        } else {
+            strcpy(Reg,"EAX");
+        }
+    }
+    *PosReg= Nfunc(byte);
 }
